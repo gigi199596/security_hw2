4ca670fb614824f24e3b7f9a3949c8305d852c0e
Victor Feyens <V.feyens@gmail.com>
2017-05-26 11:30:34 +0200

    keeping passwords

--- search.php
+++ search.php
@@ -5,8 +5,11 @@
 		echo "Search anything u want here : ";	
 	}
 ?>
-
+<html>
 	<form class="form-horizontal" method="GET">
 		<input type="text" class="form-control" name="text" value="your search here" placeholder="Search" >
+		<input id="user" type="hidden" name="login" placeholder="User" value=<?=$_GET['login']?>>
+		<input id="password" type="hidden" name="psw" placeholder="Password" value=<?=$_GET['psw']?>>
 		<button type="submit" href="" class="btn btn-primary pull-right"><i class="glyphicon glyphicon-search"></i> Log in</button>
 	</form>
+</html>
