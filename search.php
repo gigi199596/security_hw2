b6b410e94881e6a744a3f522c98e88f2621b5243
gigi199596 <Potdecolle9>
2017-05-26 11:42:31 +0200

    victor doit installer apache2

--- search.php
+++ search.php
@@ -1,8 +1,8 @@
 <?php
-	if($_GET['text']!=null)){
-		echo "U searched for : "+ $_GET['text'];	
+	if($_GET['text']!=null){
+		echo "U searched for : "+ $_GET['text'];
 	}else{
-		echo "Search anything u want here : ";	
+		echo "Search anything u want here : ";
 	}
 ?>
 <html>
@@ -10,6 +10,6 @@
 		<input type="text" class="form-control" name="text" value="your search here" placeholder="Search" >
 		<input id="user" type="hidden" name="login" placeholder="User" value=<?=$_GET['login']?>>
 		<input id="password" type="hidden" name="psw" placeholder="Password" value=<?=$_GET['psw']?>>
-		<button type="submit" href="" class="btn btn-primary pull-right"><i class="glyphicon glyphicon-search"></i> Log in</button>
+		<button type="submit" href="" class="btn btn-primary pull-right"><i class="glyphicon glyphicon-search"></i> Search</button>
 	</form>
 </html>
