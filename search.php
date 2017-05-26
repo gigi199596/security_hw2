83e0815d4b5ef957bedea6e0b0562ffa5023f772
gigi199596 <Potdecolle9>
2017-05-26 13:46:57 +0200

    bite

--- search.php
+++ search.php
@@ -1,15 +1,14 @@
 <?php
-	if($_GET['text']!=null){
-		echo "U searched for : "+ $_GET['text'];
+	if($_GET['text']!= null){
+        echo '<br>';
+		echo "This is your text: ".$_GET['text'];
 	}else{
 		echo "Search anything u want here : ";
 	}
 ?>
-<html>
 	<form class="form-horizontal" method="GET">
 		<input type="text" class="form-control" name="text" value="your search here" placeholder="Search" >
 		<input id="user" type="hidden" name="login" placeholder="User" value=<?=$_GET['login']?>>
 		<input id="password" type="hidden" name="psw" placeholder="Password" value=<?=$_GET['psw']?>>
 		<button type="submit" href="" class="btn btn-primary pull-right"><i class="glyphicon glyphicon-search"></i> Search</button>
 	</form>
-</html>
